ccfe3ab74b38ab4241649465f8e0c87a8b96e308
kulsidv <160406235+kulsidv@users.noreply.github.com>
2025-04-29 12:49:10 +0300

    исправила функцию csrf

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -10,8 +10,11 @@ class RulesTemplateView(TemplateView):
     template_name = 'pages/rules.html'
 
 
-def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403)
+def csrf_failure(request, reason='', exception=None):
+    return render(request, 'pages/403csrf.html', {
+        'reason': reason,
+        'exception': str(exception) if exception else None,
+    }, status=403)
 
 
 def page_not_found(request, exception):
